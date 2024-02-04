6015d2ea4feca81bcb4c8ca88930e9814bf6d014
santhosh <pcasd21ca076@gmail.com>
2024-02-05 00:34:47 +0530

    modify

---
+++ js/common.js
@@ -0,0 +1,16 @@
+// Load header and footer dynamically
+document.addEventListener('DOMContentLoaded', function () {
+    // Load header
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+
+    // Load footer
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
+});
